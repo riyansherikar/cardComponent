8755e944c529b6d9e3a5bc02f532bc4d51f0f037
riyansherikar <riyansherikar816@gmail.com>
2024-08-31 23:49:26 +0530

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Cards from './Cards.jsx'
+function App(){
+    return(
+        <>
+        <Cards/>
+        <Cards/>
+        <Cards/><Cards/>
+        <Cards/><Cards/>
+        <Cards/><Cards/>
+        <Cards/><Cards/>
+        <Cards/>
+        </>
+        
+    );
+}
+
+export default App;
---
+++ my-react-app/src/Cards.jsx
@@ -0,0 +1,13 @@
+
+import profilePicture from './assets/react.svg'
+function Cards(){
+    return(
+        <div className="card">
+            <img className="img" src={profilePicture} alt="profile picture" ></img>
+            <h2 className="heading">Riyan</h2>
+            <p className="text">I'm learning React</p>
+        </div>
+    );
+}
+
+export default Cards;
