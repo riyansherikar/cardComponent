271bed9a9572a302e5a7ae280788ef0a174401d5
riyansherikar <riyansherikar816@gmail.com>
2024-09-01 00:40:50 +0530

    some changes

--- my-react-app/src/Cards.jsx
+++ my-react-app/src/Cards.jsx
@@ -5,7 +5,7 @@ function Cards(){
         <div className="card">
             <img className="img" src={profilePicture} alt="profile picture" ></img>
             <h2 className="heading">Riyan</h2>
-            <p className="text">I'm learning React</p>
+            <p className="text">I'm learning React js</p>
         </div>
     );
 }
